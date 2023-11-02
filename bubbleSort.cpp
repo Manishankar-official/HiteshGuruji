6a4853568dfcdbb545f25d5e5c214ee6d86ac6da
Manishankar-official <manishankargoswami99@gmail.com>
2023-11-02 23:40:52 +0530

    corrected bubble sort not optimized

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -2,9 +2,30 @@
 using namespace std;
 
 
+
+
+
+
 void PrintA(int *arr,int n){
   for(int i=0;i<n;i++){
     cout<<arr[i]<<endl;
+  }
+}
+
+void swap(int *a,int *b){
+  int temp = *a;
+  *a = *b;
+  *b=temp;
+}
+
+void bubblesort(int *arr,int n){
+
+  for(int i=0;i<n-1;i++){
+    for(int j=0;j<n-1;j++){
+      if(arr[j]>arr[j+1]){
+        swap(&arr[j],&arr[j+1]);
+      }
+    }
   }
 }
 
@@ -12,5 +33,10 @@ void PrintA(int *arr,int n){
 int main(){
 int arr[] = {2,10,8,7};
 int n = sizeof(arr)/sizeof(*arr);
+cout<<"before swapping ::::"<<endl;
+PrintA(arr,n);
+cout<<endl;
+cout<<"After swapping::::"<<endl;
+bubblesort(arr,n);
 PrintA(arr,n);
 }
